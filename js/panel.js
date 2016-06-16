ce8f8b6192d405f47ba6ac2e34c56036f9f936aa
Juan Manuel <juanito.lg@gmail.com>
2016-06-15 21:43:51 -0600

    Se cambio el color de las cajas en la vista de consultar preguntas, se agrego la caja donde muestra el numero de preguntas que genera cada consulta

--- js/panel.js
+++ js/panel.js
@@ -71,7 +71,7 @@ function imprimirPreguntas(jsonData)
 	$('#mostrarPreguntas').empty();
 	$preguntas = JSON.parse(jsonData);
 	var cont=0;
-	var titulo = '<div class="row header-pregunta"><div class="col-lg-12 text-left"> <i class="fa fa-question-circle fa-lg" aria-hidden="true"></i> Se encontraron <bold>'+$preguntas.length+'</bold> preguntas</div> </div>';
+	var titulo = '<div class="row header-pregunta"><div class="col-lg-12 text-left"> <i class="fa fa-question-circle fa-lg" aria-hidden="true"></i> Se encontraron <strong>'+$preguntas.length+'</strong> preguntas</div> </div>';
 	$('#titulo-pregunta').html(titulo);
 
 	for(i=0; i<$preguntas.length;i++)
@@ -82,10 +82,13 @@ function imprimirPreguntas(jsonData)
 
 		//var preg='<div class="row"><div class="col-lg-12 panel panel-default cont-pregunta"><div class="panel-heading"><h3 class="panel-title badge"><bold>'+cont+'</bold></h3></div><div class="panel-body">'+ $preguntas[i].pregunta+'</div></div></div>';
 
-		var titulo_preg='<div class="row"><div class="col-lg-12 titulo-pregunta text-left">'+cont+'</div>';
-		var pregunta= '<div class="col-lg-12 panel panel-default panel-pregunta">'+ $preguntas[i].pregunta+'</div></div>';
+		var titulo_preg='<div class="row"><div class="col-lg-12 titulo-pregunta text-center">'+cont+'</div>';
+		var pregunta= '<div class="col-lg-12 panel panel-default panel-pregunta data-idpregunta="'+$preguntas[i].id_preguntas+'">'+ $preguntas[i].pregunta+'</div></div>';
 
-		var titulo_resp = '<div class="row" style="display:none"><div class="col-lg-12 titulo-respuesta"><i class="fa fa-th-list" aria-hidden="true"></i> Respuestas</div>';
+		var titulo_tipo = '<div class="row" style="display:none"><div class="col-lg-12 titulo-respuesta"><i class="fa fa-th-list" aria-hidden="true"></i> Tipo de Pregunta</div>';
+		var tipo = '<div class="col-lg-12 panel panel-default panel-respuesta"><div class="panel-body">'+$preguntas[i].id_tipo_pregunta+'</div></div>';
+
+		var titulo_resp = '<div class="col-lg-12 titulo-respuesta"><i class="fa fa-th-list" aria-hidden="true"></i> Respuestas</div>';
 		var respuestas = '<div class="col-lg-12 panel panel-default panel-respuesta"><div class="panel-body">Listado de Respuesta</div></div>';
 
 		var titulo_repaso = '<div class="col-lg-12 titulo-repaso"><i class="fa fa-pencil-square" aria-hidden="true"></i> Repaso</div>';
@@ -94,7 +97,7 @@ function imprimirPreguntas(jsonData)
 		var titulo_solucion = '<div class="col-lg-12 titulo-solucion"><i class="fa fa-check-circle" aria-hidden="true"></i> Solución</div>';
 		var solucion = '<div class="col-lg-12 panel panel-default panel-solucion"><div class="panel-body">'+$preguntas[i].solucion+'</div></div></div>';
 
-		$('#mostrarPreguntas').append(titulo_preg+pregunta+titulo_resp+respuestas+titulo_repaso+repaso+titulo_solucion+solucion);
+		$('#mostrarPreguntas').append(titulo_preg+pregunta+titulo_tipo+tipo+titulo_resp+respuestas+titulo_repaso+repaso+titulo_solucion+solucion);
 	}
 
 	$('.titulo-pregunta').on('click', function(e) {
