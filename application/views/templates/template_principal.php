cc8164938d0d5f265500f0a62283bb304a9215c9
Juan Manuel <juanito.lg@gmail.com>
2016-09-15 16:37:35 -0600

    Se agrego el código de Google analytics

--- application/views/templates/template_principal.php
+++ application/views/templates/template_principal.php
@@ -13,6 +13,17 @@
 
     <link rel="shortcut icon" href="/favicon.ico" />
 
+<!--Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-84293407-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css')?>">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
