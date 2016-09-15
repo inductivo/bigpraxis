58bab6358ecd4f0dc5c14d114211786e7b942dea
Juan Manuel <juanito.lg@gmail.com>
2016-09-15 11:41:46 -0600

    Cambios en la optimización del sitio web, css, .htaccess

--- application/views/templates/template_test.php
+++ application/views/templates/template_test.php
@@ -14,6 +14,7 @@
     <!-- Bootstrap Core CSS -->
     <link href="<?= base_url('css/bootstrap.min.css')?>" rel="stylesheet">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat|Lato:400,700,300,900|Roboto:400,100,300,700,500,900' rel='stylesheet' type='text/css'>
 
 
     <!-- Custom CSS -->
